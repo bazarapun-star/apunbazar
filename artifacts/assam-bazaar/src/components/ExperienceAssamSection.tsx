652ca193447e80a35a4ece5bff51ae3f595365f1
bazarapun <bazarapun@gmail.com>
2026-07-02 15:03:46 +0530

    Update ExperienceAssamSection.tsx

--- artifacts/assam-bazaar/src/components/ExperienceAssamSection.tsx
+++ artifacts/assam-bazaar/src/components/ExperienceAssamSection.tsx
@@ -6,7 +6,7 @@ const BG   = "#f5f0e8";
 
 const COLLAGE_IMGS = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLDPxd-dWlvp9iM3pnxr-HUa0VN_oEGAC7-5L-6GBToJFt6eFcM-as1H4C&s=10",
-  "https://few-ivory-5rwrxvwj.edgeone.dev/file_0000000007c471f881c68e5aa72c84e7.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTParCuXRWg4CJhOHz3UhS5KqeqYVo5L8NScdU8nVh1x92xam8PctswHs2h&s=10",
   "https://sandpebblestours.com/wp-content/uploads/2018/11/assam-1.jpg",
   "https://www.travelandtourworld.com/wp-content/uploads/2026/04/Assam-1-850x567.jpg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTParCuXRWg4CJhOHz3UhS5KqeqYVo5L8NScdU8nVh1x92xam8PctswHs2h&s=10",
